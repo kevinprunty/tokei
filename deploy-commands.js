bb2a5ad59b1c51f3c18e5cc201c4b9761a2cd10e
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-21 20:11:31 -0400

    Moved clientID to ENV

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,7 +22,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 	try {
 		console.log(`Started refreshing slash commands.`);
 
-		await rest.put(Routes.applicationCommands(clientId), { body: commands })
+		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 		.then(() => console.log('Successfully registered application commands.'))
 		.catch(console.error);
 
