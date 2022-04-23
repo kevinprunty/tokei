6e316fdb29790a990590740c5c3d333f8ffa2f15
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-23 13:30:05 -0400

    Removed most database functionality, will be retried in the future

--- data/database/driver.js
+++ data/database/driver.js
@@ -6,30 +6,10 @@ const EventDrawing = require('./models/EventDrawing.js');
 
 // Actual database functions
 const eventDrawing = {
-    createNewEntry : function(event, data, interaction) {
-        if (!interaction) return;
-        const newEntry = new EventDrawing({
-            artist: interaction.member,
-            number: data.number,
-            name: data.name,
-            series: data.series,
-            event: event,
-            status: {
-                received: false,
-                slideshow: false 
-            }
-        });
-
-        return newEntry.save(function(err){
-            if (err) console.log(err.message);
-        });
-    },
-    debugDeleteAllEntries : function(){
-        EventDrawing.remove({}, ()=>console.log("Error! For some reason!"));
-    }
+   
 }
 
 
  module.exports = {
-     eventDrawing,
+    eventDrawing
  }
