761b8e3c1ce1b83db2dd828c078ca7adf05599b4
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-25 19:14:35 -0400

    Mysterious question mark

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.once('ready', async () => {
     console.log('Conencted to the database');
 
     // Set command permissions
-    
+
     // Gacha admin
     // Role: : 968274336999936100
     // Command ID: 968273941573541948
@@ -52,7 +52,7 @@ client.once('ready', async () => {
         }
     ]
 
-    await client.guilds.cache.get(process.env.GUILD_ID).commands.permissions.set({ fullPermissions })
+    await client.guilds.cache.get(process.env.GUILD_ID)?.commands.permissions.set({ fullPermissions })
 
 });
 
