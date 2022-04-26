ee0a68e71f5d7792d42bcc66156cdd871a1ff1e2
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-25 20:13:05 -0400

    Still troubleshooting gacha

--- commands/gacha.js
+++ commands/gacha.js
@@ -6,8 +6,8 @@ module.exports = {
 		.setName('gacha')
 		.setDescription('Pull from the EXP Gacha!'),
 	async execute(interaction) {
-        const randomGachaRarityList = await gacha.getRandomGachaRarity();
-        if (randomGachaRarityList.length = 0){
+        const randomGachaRarityList = await gacha.getRandomGachaRarity();)
+        if (randomGachaRarityList.length == 0){
             return interaction.reply({
                 content: "There isn't anything in that rarity!", 
                 ephemeral: true
