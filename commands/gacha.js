a330f50d37c17bad3ad29b672c519c1187de8da8
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-25 20:18:29 -0400

    Still troubleshooting gacha

--- commands/gacha.js
+++ commands/gacha.js
@@ -15,7 +15,7 @@ module.exports = {
         }
         const randomIndex = Math.floor(Math.random * randomGachaRarityList.length);
         const gachaPull = randomGachaRarityList[randomIndex];
-        console.log(gachaPull);
+        console.log(randomGachaRarityList, randomIndex, gachaPull);
 		await interaction.reply({
 			content:`You pulled #${gachaPull.gachaId} ${gachaPull.name} (${gachaPull.rarity})! ${gachPull.description}`,
 		})
