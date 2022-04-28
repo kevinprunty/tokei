14f18831d6deaa7fa5845299f551104511348fcc
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-28 16:32:06 -0400

    Support command.

---
+++ commands/support.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const supportLinks = [
+    {
+        name: 'PayPal',
+        link: 'http://paypal.me/toasterbrains'
+    }, 
+    {
+        name: 'Ko-fi', 
+        link: 'https://ko-fi.com/toasterbrains'
+    }
+]
+
+
+module.exports = { 
+    data: new SlashCommandBuilder()
+        .setName('support')
+        .setDescription('Show links that support Toaster and the bot.'),
+    async execute(interaction){
+
+        let supportTextArray = supportLinks.map(link => `[${link.name}: ${link.link}]`);
+
+        const responseText = `Thank you for using the bot so much!\n
+        You don't have to support me, I will still make bot content free forever, but here are a few links where you can donate if you want.\n\n
+        ${supportTextArray.join('\n')}`
+
+        await interaction.reply({
+            content: responseText
+        })
+
+    }
+}
