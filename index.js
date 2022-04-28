41bc17115543f0eb4a472c8374a2ad32d7e66342
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-27 20:41:35 -0400

    Commented out permissions error

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ client.once('ready', async () => {
         }
     ]
 
-    console.log("Setting permissions");
-    await client.guilds.cache.get(process.env.GUILD_ID)?.commands.permissions.set({ fullPermissions })
-    console.log("Permissions set");
+    // console.log("Setting permissions");
+    // await client.guilds.cache.get(process.env.GUILD_ID)?.commands.permissions.set({ fullPermissions })
+    // console.log("Permissions set");
 });
 
 
