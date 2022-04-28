f211d7f87a7273b4504df874c3f8cac128fbc0ca
Kevin Prunty <kevin.prunty95@gmail.com>
2022-04-27 22:02:26 -0400

    Pat pat's last stand stand

---
+++ commands/patpatimage.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {MessageAttachment} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('patpatimage')
+		.setDescription('Patpats an image. Takes an image url parameter.')
+		.addStringOption(option => option.setName('url').setDescription('The image url to patpat').setRequired(true)),
+	async execute(interaction) {
+		const url = interaction.options.getString('url');
+		const urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+		if (!url.match(urlRegex)){
+			return interaction.reply({
+				content:`Your URL isn't valid.`,
+				ephemeral: true
+			})
+		}
+
+
+		const outputUrl = `https://api.jeyy.xyz/image/patpat?image_url=${url}.gif`
+		const attachment = new MessageAttachment(outputUrl, 'patpat.gif');
+
+
+
+		await interaction.editReply({
+			attachments: [attachment]
+		})
+	},
+};
