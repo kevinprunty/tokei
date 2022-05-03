76f1ad9a1f3013fd197283d0f8da604733a96d86
Kevin Prunty <kevin.prunty95@gmail.com>
2022-05-03 19:33:34 -0400

    Removed RarityChoicesArray

--- commands/gachaadmin.js
+++ commands/gachaadmin.js
@@ -10,15 +10,6 @@ const rarityChoices = [
     {name: 'SSS', value: 'SSS'},
 ]
 
-const rarityChoicesArray = [
-    ["C", "C"],
-    ["B", "B"],
-    ["A", "A"],
-    ["S", "S"],
-    ["SS", "SS"],
-    ["SSS", "SSS"],
-  ]
-
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -37,13 +28,12 @@ module.exports = {
                     .setName('rarity')
                     .setDescription('Rarity for item. [C, B, A, S, SS, SSS]')
                     .setRequired(true)
-                    // .addChoices(rarityChoices[0])
-                    // .addChoices(rarityChoices[1])
-                    // .addChoices(rarityChoices[2])
-                    // .addChoices(rarityChoices[3])
-                    // .addChoices(rarityChoices[4])
-                    // .addChoices(rarityChoices[5])
-                    .addChoices(rarityChoicesArray))
+                    .addChoices(rarityChoices[0])
+                    .addChoices(rarityChoices[1])
+                    .addChoices(rarityChoices[2])
+                    .addChoices(rarityChoices[3])
+                    .addChoices(rarityChoices[4])
+                    .addChoices(rarityChoices[5]))
                 .addStringOption(option => option
                     .setName('description')
                     .setDescription('The short description for the item.')
